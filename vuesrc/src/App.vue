1cc3f289a9960bd1184c2d6fb89e02674fc65c29
Andy-from-fs <735188273@qq.com>
2017-09-05 16:22:35 +0800

    init

---
+++ vuesrc/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+   
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+body{
+  padding: 0 50px;
+  background: url(./assets/background.png);
+}
+</style>
